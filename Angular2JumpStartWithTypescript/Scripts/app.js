6139f7b1c285c59958f2b09f1ebfb664dd13d4ca
Jose George <jose.george@sterlingventures.net>
2017-01-05 17:30:52 +0000

    Added the project for first time

---
+++ Angular2JumpStartWithTypescript/Scripts/app.js
@@ -0,0 +1,31 @@
+﻿var btn1State = "no value";
+var btn2State = "no value";
+
+$(document).ready(function () {
+    $('#area1').text("Button1 Last Updated    ");
+    $('#area1').append(btn1State);
+    $('#area2').text("Button2 Last Updated    ");
+    $('#area2').append(btn2State);
+});
+
+function buttonOneLogic() {
+    var seconds = new Date().getTime() / 1000;
+    btn1State = seconds;
+
+    $('#area1').show();
+    $('#area1').text("Button1 Last Updated   ");
+    $('#area1').append(btn1State);
+
+}
+
+function buttonTwoLogic() {
+    var seconds = new Date().getTime() / 1000;
+    btn2State = seconds;
+
+    $('#area2').show();
+    $('#area2').text("Button2 Last Updated   ");
+    $('#area2').append(btn2State);
+}
+
+
+
